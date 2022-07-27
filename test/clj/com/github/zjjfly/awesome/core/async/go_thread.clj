056645434db2141b7de3b0bc531e4949667b2077
zijunjie <zjjblue@gmail.com>
2022-07-27 16:22:59 +0800

    use Criterium for benchmark instead of time

--- test/clj/com/github/zjjfly/awesome/core/async/go_thread.clj
+++ test/clj/com/github/zjjfly/awesome/core/async/go_thread.clj
@@ -1,21 +1,21 @@
 (ns com.github.zjjfly.awesome.core.async.go-thread
   (:require [clojure.test :refer :all]
             [com.github.zjjfly.awesome.core.async.examples :refer [async-go-factorizer
-                                                                     async-thread-factorizer
-                                                                     async-with-pipeline
-                                                                     launch-go-blocking-and-compute
-                                                                     launch-thread-blocking-and-compute]]))
-(defmacro exec-time [expr]
-  `(let [start-time# (System/currentTimeMillis)
-         ret# ~expr]
-     (- (System/currentTimeMillis) start-time#)))
+                                                                   async-thread-factorizer
+                                                                   async-with-pipeline
+                                                                   launch-go-blocking-and-compute
+                                                                   launch-thread-blocking-and-compute]]
+            [criterium.core :as criterium]))
+
+(defn mean-time [f]
+  (* 1000 (first (:mean (criterium/quick-benchmark* f {})))))
 
 ;使用core.async的go,thread,pipeline来并行调用factorizer,它们的性能应该是差不多的
 (deftest factorizer-test
   (let [
-        go-time (exec-time (async-go-factorizer (repeat 1000 (* 29 982451653)) 8))
-        thread-time (exec-time (async-thread-factorizer (repeat 1000 (* 29 982451653 )) 8))
-        pipeline-time (exec-time (async-with-pipeline (repeat 1000 (* 29 982451653 )) 8))
+        go-time (mean-time #(async-go-factorizer (repeat 1000 (* 29 982451653)) 8))
+        thread-time (mean-time #(async-thread-factorizer (repeat 1000 (* 29 982451653)) 8))
+        pipeline-time (mean-time #(async-with-pipeline (repeat 1000 (* 29 982451653)) 8))
         ]
     (is (< (Math/abs (- go-time thread-time)) 50))
     (is (< (Math/abs (- go-time pipeline-time)) 50))))
@@ -23,10 +23,8 @@
 ;go和thread在有阻塞操作的时候,thread的性能比较好的
 (deftest block-test
   (let [
-        go-time (exec-time (launch-go-blocking-and-compute 32 16))
-        thread-time (exec-time (launch-thread-blocking-and-compute 32 16))
+        go-time (mean-time #(launch-go-blocking-and-compute 32 16))
+        thread-time (mean-time #(launch-thread-blocking-and-compute 32 16))
         ]
-    (println go-time)
-    (println thread-time)
     (is (> (- go-time thread-time) 500))))
 
