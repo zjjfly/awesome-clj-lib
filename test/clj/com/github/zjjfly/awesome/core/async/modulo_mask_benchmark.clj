6e27d3cefd614170e0989df46d42e62e83d864fb
zijunjie <zjjblue@gmail.com>
2022-07-29 12:09:51 +0800

    benchmark for modulo mask

---
+++ test/clj/com/github/zjjfly/awesome/core/async/modulo_mask_benchmark.clj
@@ -0,0 +1,19 @@
+(ns com.github.zjjfly.awesome.core.async.modulo-mask-benchmark
+  (:require [criterium.core :as criterium]
+            [primitive-math :as primitive-math]))
+
+;refer to this blog: http://psy-lob-saw.blogspot.com/2014/11/the-mythical-modulo-mask.html
+
+(primitive-math/use-primitive-operators)
+
+(defn opt-modulo
+  ^long [^long x ^long y]
+  (bit-and x (- y 1)))
+
+(opt-modulo 7 4)
+
+(let [mean (first (:mean (criterium/benchmark* #(rem 7 4) {})))]
+  (println (str "mean time of %: " mean)))
+
+(let [mean (first (:mean (criterium/benchmark* #(opt-modulo 7 4) {})))]
+  (println (str "mean time of opt-modulo: " mean)))
